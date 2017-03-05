c52fdd43d5a6ddfc005640ed14e9d19e1c548432
Eugene <eugene.gubar@gmail.com>
2017-03-05 08:07:46 +0200

    Finish realize session, break, click

--- js/script.js
+++ js/script.js
@@ -52,6 +52,9 @@ function gradients() {
  * @param {number} s - interval session seconds
  */
 function clickTimer() {
+  // ===========================================================================
+  // click main time
+  // ===========================================================================
   $('.time').on('click', function (e) {
     e.preventDefault();
     if (interval >= 1) {
@@ -61,6 +64,54 @@ function clickTimer() {
       startTimer(sessionLengthMin * 60, $('.time'));
     }
   });
+  // ===========================================================================
+  // click break length - plus
+  // ===========================================================================
+  $('.break-plus').on('click', function (e) {
+    e.preventDefault();
+    if (breakLengthMin === 60) {
+      breakLengthMin = 60;
+    } else {
+      breakLengthMin += 1;
+    }
+    $('.break-minute').text(breakLengthMin);
+  });
+  // ===========================================================================
+  // click break length - minus
+  // ===========================================================================
+  $('.break-minus').on('click', function (e) {
+    e.preventDefault();
+    if (breakLengthMin === 1) {
+      breakLengthMin = 1;
+    } else {
+      breakLengthMin -= 1;
+    }
+    $('.break-minute').text(breakLengthMin);
+  });
+  // ===========================================================================
+  // click session length - plus
+  // ===========================================================================
+  $('.session-plus').on('click', function (e) {
+    e.preventDefault();
+    if (sessionLengthMin === 120) {
+      sessionLengthMin = 120;
+    } else {
+      sessionLengthMin += 1;
+    }
+    $('.session-minute').text(sessionLengthMin);
+  });
+  // ===========================================================================
+  // click session length - minus
+  // ===========================================================================
+  $('.session-minus').on('click', function (e) {
+    e.preventDefault();
+    if (sessionLengthMin === 1) {
+      sessionLengthMin = 1;
+    } else {
+      sessionLengthMin -= 1;
+    }
+    $('.session-minute').text(sessionLengthMin);
+  });
 }
 
 /**
@@ -68,7 +119,7 @@ function clickTimer() {
  * @param {number} seconds - set seconds
  * @param {obj} element - set tag element $() selector
  */
-function startTimer(seconds, element) {
+function startTimer(seconds, element, b) {
   var minutes, seconds, timer = seconds;
   interval = setInterval(function () {
     minutes = parseInt(timer / 60, 10)
@@ -81,28 +132,33 @@ function startTimer(seconds, element) {
 
     if (--timer < 0) {
       timer = seconds;
+      stopTime();
+      interval = 0;
+      if (b === true) {
+        stopTime();
+        interval = 0;
+      } else {
+        breakSession(breakLengthMin);
+      }
     }
   }, 1000);
 }
 
+/**
+ * Function stopTime - clear interval
+ */
 function stopTime() {
   clearInterval(interval);
 }
 
-/*
-
-function min(m) {
-  seconds = m * 60;
+/**
+ * Function breakSession - run function startTimer with 3-rd arguments (option)
+ * for the implementation of a single run of the function
+ * @param {number} b - seconds 
+ */
+function breakSession(b) {
+  startTimer(b * 60, $('.time'), true);
 }
-
-
-function break(b) {
-
-}
-
-function click() {
-  
-}*/
 
 gradients();
 clickTimer();
